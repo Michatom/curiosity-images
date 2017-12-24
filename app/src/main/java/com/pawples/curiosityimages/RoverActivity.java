a9490b1bfcc4bc32ac193a63befad11a48068e49
PawelPleskaczynski <hamstersztyk@gmail.com>
2017-12-24 16:12:17 +0100

    1.0-final

--- app/src/main/java/com/pawples/curiosityimages/RoverActivity.java
+++ app/src/main/java/com/pawples/curiosityimages/RoverActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.GenericTransitionOptions;
+import com.bumptech.glide.Glide;
 
 public class RoverActivity extends AppCompatActivity {
 
@@ -14,6 +18,18 @@ public class RoverActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rover);
         CardView cardCuriosity = findViewById(R.id.card);
         CardView cardOpportunity = findViewById(R.id.card2);
+
+        ImageView imgCuriosity = findViewById(R.id.squareImg);
+        ImageView imgOpportunity = findViewById(R.id.squareImg2);
+
+        Glide.with(this)
+                .load(R.drawable.curiosity_rover)
+                .transition(GenericTransitionOptions.with(R.anim.img_rover))
+                .into(imgCuriosity);
+        Glide.with(this)
+                .load(R.drawable.opportunity_rover)
+                .transition(GenericTransitionOptions.with(R.anim.img_rover))
+                .into(imgOpportunity);
 
         cardCuriosity.setOnClickListener(new View.OnClickListener() {
             @Override
